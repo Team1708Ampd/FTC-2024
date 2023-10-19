c53d6620eaca3304e558c7aa3df9b449b1c792a3
dcipicchio <dac221@pitt.edu>
2023-10-19 18:12:27 -0400

    Added the setup for Vision Portal/basic apriltag detection

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleopOpMode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleopOpMode.java
@@ -5,6 +5,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.concurrent.TimeUnit;
+
 @TeleOp(name = "Main Teleop OPMode")
 public class MainTeleopOpMode extends LinearOpMode {
 
@@ -13,8 +23,18 @@ public class MainTeleopOpMode extends LinearOpMode {
     private DcMotor frontLeftDrive;
     private DcMotor backLeftDrive;
 
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private AprilTagDetection desiredTag = null;
+
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
     @Override
     public void runOpMode() {
+        initAprilTag();
+
+        if (USE_WEBCAM)
+            setManualExposure(6, 250);
+
         frontRightDrive = hardwareMap.get(DcMotor.class, "Front Right");
         backRightDrive = hardwareMap.get(DcMotor.class, "Back Right");
         frontLeftDrive = hardwareMap.get(DcMotor.class, "Front Left");
@@ -62,5 +82,61 @@ public class MainTeleopOpMode extends LinearOpMode {
         frontRightDrive.setPower(rightFrontPower);
         backLeftDrive.setPower(leftBackPower);
         backRightDrive.setPower(rightBackPower);
+    }
+
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Create the vision portal by using a builder.
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+
+    /*
+     Manually set the camera gain and exposure.
+     This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+    */
+    private void    setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure(exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+        }
     }
 }
